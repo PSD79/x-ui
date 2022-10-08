f21db5a6b39831479fe508e26d3c40e70b5d9c1f
AlphaCyber <65259497+PSD79@users.noreply.github.com>
2022-10-08 18:35:22 +0330

    Update db.go

--- database/db.go
+++ database/db.go
@@ -1,7 +1,8 @@
 package database
 
 import (
-	"gorm.io/driver/mysql"
+// 	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"io/fs"
@@ -59,7 +60,8 @@ func InitDB(dbPath string) error {
 	c := &gorm.Config{
 		Logger: gormLogger,
 	}
-	db, err = gorm.Open(mysql.Open(dbPath), c)
+// 	db, err = gorm.Open(mysql.Open(dbPath), c)
+	db, err = gorm.Open(sqlite.Open(dbPath), c)
 	if err != nil {
 		return err
 	}
