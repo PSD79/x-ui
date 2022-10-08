a8c1407284fffa90c724c8cf0f42203602cc6f60
AlphaCyber <65259497+PSD79@users.noreply.github.com>
2022-10-08 22:54:39 +0330

    Update config.go

--- config/config.go
+++ config/config.go
@@ -46,16 +46,16 @@ func IsDebug() bool {
 }
 
 
-// func GetDBPath() string {
-// 	return fmt.Sprintf("/etc/%s/%s.txt", GetName(), GetName())
-// }
-
 func GetDBPath() string {
+	return fmt.Sprintf("/etc/%s/%s.db", GetName(), GetName())
+}
+
+// func GetDBPath() string {
 // 	var dat string
 // 	dat, err := os.ReadFile("/etc/x-ui/mysql.txt")
 // 	if err != nil {
 // 		return err
 //     	}
 //     	return fmt.Print(dat)
-	return fmt.Sprintf("proxy:pouria13791379@tcp(178.22.121.12:3306)/proxies?charset=utf8mb4")
-}
+// 	return fmt.Sprintf("proxy:pouria13791379@tcp(178.22.121.12:3306)/proxies?charset=utf8mb4")
+// }
